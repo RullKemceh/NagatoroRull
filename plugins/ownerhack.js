b917530ae7b14b84fe5131b16e42847227620772
RullKemceh <103581067+RullKemceh@users.noreply.github.com>
2022-05-23 16:26:56 +0700

    Add files via upload

---
+++ plugins/ownerhack.js
@@ -0,0 +1,29 @@
+const rewards = {
+    exp: 900000000000000,
+    money: 9000000000000,
+    potion: 9000000000000,
+    mythic: 900000000000,
+    legendary: 900000000,
+}
+
+const cooldown = 2592000000
+let handler = async (m) => {
+    let user = global.db.data.users[m.sender]
+    if (new Date - user.lastmonthly < cooldown) throw `You have already claimed this monthly claim, wait for *${((user.lastmonthly + cooldown) - new Date()).toTimeString()}*`
+    let text = ''
+    for (let reward of Object.keys(rewards)) if (reward in user) {
+        user[reward] += rewards[reward]
+        text += `*+${rewards[reward]}* ${rpg.emoticon(reward)}${reward}\n`
+    }
+    conn.sendButton(m.chat,'*––––––『 OWNER HACK 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Menu', '.menu']],m)
+    user.lastmonthly = new Date * 1
+}
+handler.help = ['ownerhack']
+handler.tags = ['owner']
+handler.command = /^(ownerhack)$/i
+
+handler.owner = true
+
+handler.cooldown = cooldown
+
+export default handler
