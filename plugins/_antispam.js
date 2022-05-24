277d9d7d83d1ae2201a38201068ca2785b7d597c
RullKemceh <103581067+RullKemceh@users.noreply.github.com>
2022-05-24 12:35:14 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
+            if (this.spam[m.sender].count > 100) {
                 //global.db.data.users[m.sender].banned = true
                 m.reply('*Jangan Spam!!*')
             }
